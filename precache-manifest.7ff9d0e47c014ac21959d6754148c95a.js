11c82ec88219b12ca9f5e59157d19b4a33633459
Danny Chen <zhuodannychen@gmail.com>
2021-12-31 16:39:36 -0600

    build files

---
+++ precache-manifest.7ff9d0e47c014ac21959d6754148c95a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "795d72e80a86c035919621968fdc1b85",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "d485d4ca45b715abdb11",
+    "url": "/Portfolio/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "ad8915f3ec6d0d18ccd8",
+    "url": "/Portfolio/static/css/main.e7206422.chunk.css"
+  },
+  {
+    "revision": "d485d4ca45b715abdb11",
+    "url": "/Portfolio/static/js/2.273adfc2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Portfolio/static/js/2.273adfc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad8915f3ec6d0d18ccd8",
+    "url": "/Portfolio/static/js/main.ea560b11.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "8df1775bc8017a1e88428d18edda80bd",
+    "url": "/Portfolio/static/media/chinese_calligraphy.8df1775b.jpeg"
+  },
+  {
+    "revision": "4ef97c0f5ab806260d60e2408f59b009",
+    "url": "/Portfolio/static/media/danny1_4_16_2020.4ef97c0f.jpg"
+  },
+  {
+    "revision": "ec9bc58f6cef07a156fd6ca0d672bf4b",
+    "url": "/Portfolio/static/media/pwsh_invitational.ec9bc58f.png"
+  },
+  {
+    "revision": "59abab78cd837ff8ca1cd4758b23e625",
+    "url": "/Portfolio/static/media/stock.59abab78.png"
+  },
+  {
+    "revision": "d91e653bb562c61c886b90958a17f15a",
+    "url": "/Portfolio/static/media/zhuo_chen_resume.d91e653b.pdf"
+  }
+]);

9d83623b227149d351fc02090123614bae2d14c8
Danny Chen <zhuodannychen@gmail.com>
2021-12-31 16:16:59 -0600

    build

---
+++ precache-manifest.4b4066480f3eeaaf8fc745e8f9196257.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46b3fc8d970bac8a5c2d38aedf88681",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06ed055d4fde83de161c",
+    "url": "/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "85fb9a4fbd8f7982f417",
+    "url": "/static/css/main.0a89d4f2.chunk.css"
+  },
+  {
+    "revision": "06ed055d4fde83de161c",
+    "url": "/static/js/2.bdbc493e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.bdbc493e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85fb9a4fbd8f7982f417",
+    "url": "/static/js/main.e9dc17f3.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "4ef97c0f5ab806260d60e2408f59b009",
+    "url": "/static/media/danny1_4_16_2020.4ef97c0f.jpg"
+  },
+  {
+    "revision": "836a9d12e9d5a7aa2e3648f7b01a9846",
+    "url": "/static/media/editorial.836a9d12.pdf"
+  },
+  {
+    "revision": "ec9bc58f6cef07a156fd6ca0d672bf4b",
+    "url": "/static/media/pwsh_invitational.ec9bc58f.png"
+  }
+]);
